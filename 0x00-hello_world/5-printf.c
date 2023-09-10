10e2a9a33c8651d33843fb81cf14b8991db82c18
EmmanuelsegunF <shegstix@gmail.com>
2023-09-10 12:31:26 -0700

    Hello worldd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (succes)
+ * main - program begins here
+ * Return: Always 0
  */
 int main(void)
-{	printf("with proper grammar, but the outcome is a piece of art\n");
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+}	
